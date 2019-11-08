3aae831ef1edd86905deda095b5ce45e5919955b
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-11-08 12:12:44 -0700

    some sort of progress

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,32 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
-    
-    pass
+    th = 0
+    word = list(word)
+    count = 0
+
+    if word == []:
+        return 0
+
+    def recur(word):
+        nonlocal count
+        nonlocal th
+
+        if count == len(word) - 1:
+            print(th)   # <- when I change this line to return th it doesn't return anything
+            
+        else:
+            print(count)
+            if word[count] == 't' and word[count + 1] == 'h':
+                th += 1
+            count += 1
+            recur(word)
+
+    return recur(word)
+
+
+print(count_th(''))
+print(count_th('abcthxyz'))
+print(count_th('abcthefthghith'))
+print(count_th('thhtthht'))
+print(count_th('THtHThth'))
